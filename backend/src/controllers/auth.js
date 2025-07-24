e64b3c7d4c43cfa86124c13cb85655621a06fd31
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2025-07-25 02:35:21 +0530

    auth api created

---
+++ backend/src/controllers/auth.js
@@ -0,0 +1,66 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ error: "Email and password are required" });
+    }
+
+    const existingUser = await User.findOne({ email }).lean();
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json({
+      message: "user created successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message || "An error occurred while signing up",
+    });
+  }
+};
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ error: "Email and password are required" });
+    }
+    const user = await User.findOne({ email }).lean();
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ error: "Invalid password" });
+    }
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET
+    );
+    res.status(200).json({ token, email: user.email, id: user._id });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message || "An error occurred while signing in",
+    });
+  }
+};
+
+module.exports = {
+  signup,
+  signin,
+};
--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { stock } = require("../controllers/yahoofinance");
 const { searchStock } = require("../controllers/searchApi");
+const { signup, signin } = require("../controllers/auth");
 const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
 
 router.get("/stock", stock);
 router.get("/searchStock", searchStock);
