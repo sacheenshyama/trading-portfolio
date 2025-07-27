61abd3ce6384b4b68799f0b888c9eb96204e086b
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2025-07-28 02:53:38 +0530

    small change in backend

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -53,9 +53,7 @@ const signin = async (req, res) => {
       process.env.JWT_SECRET
     );
     res.cookie("jwtToken", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      httpOnly: false,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({ token, email: user.email, id: user._id });
