7fc1e3c0dc92961cd337e1a2e9cf6fbbbd100859
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2025-07-29 13:29:28 +0530

    node module removed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
+require("dotenv").config();
+
 const cors = require("cors");
 const app = express();
 const Routes = require("./routes/route");
-// const morgon=require('m')
 const morgan = require("morgan");
 const { METHODS } = require("http");
 
 const corsOptions = {
-  origin:
-    process.env.FRONTEND_URL || "https://trading-portfolio-one.vercel.app",
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
