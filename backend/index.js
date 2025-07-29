c6f64da1f2b3acb701205f2581d606f132442593
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2025-07-29 16:50:22 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
-require('dotenv').config();
-const http=require('http');
-const mongoose=require('mongoose');
-const app=require('./src/app');
+require("dotenv").config();
+const http = require("http");
+const mongoose = require("mongoose");
+const app = require("./src/app");
 
-const MONGO_URL=process.env.MONGO_URL;
-
-mongoose.connect(MONGO_URL).then(()=>{
-    const server=http.createServer(app);
-    server.listen(4000,()=>{
-        console.log('Server is running on port 4000');
-    })
-    console.log('Connected to MongoDB');
-}).catch((err)=>{
-    console.error('Error connecting to MongoDB:', err);
+const MONGO_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 4000;
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    const server = http.createServer(app);
+    server.listen(PORT, () => {
+      console.log("Server is running on port 4000");
+    });
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
     process.exit(1);
-});
+  });
