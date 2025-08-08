63c4f7cc48d47f81f9947848e076d159dece7e9e
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2025-08-08 18:22:37 +0530

    backend

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -8,7 +8,8 @@ const authMiddleware = async (req, res, next) => {
   const token =
     req.cookies.jwtToken || req.header("Authorization")?.replace("Bearer ", "");
   if (!token) {
-    return res.status(401).json({  error: "Access denied" });
+    console.log(req);
+    return res.status(401).json({ error: "Access denied" });
   }
 
   try {
